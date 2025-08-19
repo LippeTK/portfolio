1740f96b64bb56576cc3d36b4a42973a7e4e2ef2
Felipe <104037790+LippeTK@users.noreply.github.com>
2025-08-19 16:27:41 -0300

    Feat: added openLink function to buttons

--- my-app/src/components/Button/Button.tsx
+++ my-app/src/components/Button/Button.tsx
@@ -4,12 +4,15 @@ import { View, Text, TouchableOpacity } from "react-native";
 import { styles } from "./ButtonStyles";
 interface ButtonProps {
   label: string;
+  onPress?: () => void;
 }
 
-export function Button({ label }: ButtonProps) {
+export function Button({ label, onPress }: ButtonProps) {
   return (
-    <View style={styles.button}>
-      <Text style={styles.buttonText}>{label}</Text>
-    </View>
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <View>
+        <Text style={styles.buttonText}>{label}</Text>
+      </View>
+    </TouchableOpacity>
   );
 }
--- my-app/src/pages/Home.tsx
+++ my-app/src/pages/Home.tsx
@@ -6,6 +6,13 @@ import { Button } from "../components/Button/Button";
 import { StatusBar } from "expo-status-bar";
 import { Profile } from "../components/ProfilePicture/Profile";
 
+import * as Linking from "expo-linking";
+
+const openLink = (url: string) => {
+  Linking.openURL(url).catch((err) => {
+    console.error("Erro ao abrir link:", err);
+  });
+};
 export function Home() {
   return (
     <View style={styles.container}>
@@ -13,9 +20,20 @@ export function Home() {
         <Profile />
       </View>
       <View style={styles.buttonsContainer}>
-        <Button label={"LinkedIn"} />
-        <Button label={"GitHub"} />
-        <Button label={"E-mail"} />
+        <Button
+          label="LinkedIn"
+          onPress={() =>
+            openLink("https://www.linkedin.com/in/felipe-avila-carvalho/")
+          }
+        />
+        <Button
+          label="GitHub"
+          onPress={() => openLink("https://github.com/LippeTK")}
+        />
+        <Button
+          label="Whatsapp"
+          onPress={() => openLink("https://wa.me/5551991900860?text=Oi!")}
+        />
       </View>
       <StatusBar style="auto" />
     </View>
